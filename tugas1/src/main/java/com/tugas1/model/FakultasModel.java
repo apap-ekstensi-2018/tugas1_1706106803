276c753033951c330af0c4c8474160a26e4bc965
lidianatalia <lidiamanik@outlook.com>
2018-04-03 23:37:33 +0700

    add

---
+++ tugas1/src/main/java/com/tugas1/model/FakultasModel.java
@@ -0,0 +1,16 @@
+package com.tugas1.model;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class FakultasModel {
+	private int id;
+	private String kode_fakultas;
+	private String nama_fakultas;
+	private int id_univ;
+	private String nama_univ;
+}
